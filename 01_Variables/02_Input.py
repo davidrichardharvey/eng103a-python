497efb24e85259414e0d0b87239f517455b86818
David Harvey <dharvey@spartaglobal.com>
2022-01-24 11:38:07 +0000

    Added input session

---
+++ 01_Variables/02_Input.py
@@ -0,0 +1,11 @@
+name = input("What is your name?\n")
+print("Hello, " + name)
+
+# Ask 3 questions
+# Use the stores variables somehow, and print out a response
+# Can you ask questions with numerical answers?
+
+age = int(input("How old are you?\n"))
+# print("You are " + str(age) + " years old")
+print(f"You are {age} years old")  # formatted string
+print(f"Next year you will be {age + 1} years old")
