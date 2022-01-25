73cade79cfe6d36fbeb78d9ea43ca87e4f434a50
David Harvey <dharvey@spartaglobal.com>
2022-01-25 14:34:35 +0000

    Finished functions

---
+++ 04_Functions/calculator.py
@@ -0,0 +1,23 @@
+# The following functions
+# Each should take 2 arguments
+# And return the appropriate calculation
+
+# add
+# subtract
+# multiply
+# divide
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
